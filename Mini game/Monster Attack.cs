bef7d01a02f6b99d5039c39d5fffba3b6ecf5983
marti <martinek.musil1@gmail.com>
2026-06-02 20:44:40 +0200

    list

--- Mini game/Monster Attack.cs
+++ Mini game/Monster Attack.cs
@@ -2,5 +2,18 @@ namespace Mini_game;
 
 public class Monster_Attack
 {
+    string Monster;
+    public int Monster_hp;
+
+    public Monster_Attack(string monster, int monster_hp)
+    {
+        Monster = monster;
+        Monster_hp = monster_hp;
+    }
+
+    List<string> names = new List<string>();
     
+    
+
+    Monster_Attack monstrum = new Monster_Attack();
 }
